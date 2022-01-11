a0499c1023c6614b5a8ab01f02f84ae4fd3aed0d
Jelani Thomas <jelani13@icloud.com>
2022-01-11 13:18:13 -0500

    binding implemented in MainActivity.kt

--- app/src/main/java/com/example/geocoder/MainActivity.kt
+++ app/src/main/java/com/example/geocoder/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.geocoder
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.geocoder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.searchButton.setOnClickListener{ fetchLocation()}
+        binding.enteredDestination.setOnKeyListener{ view, keyCode, _ ->handleKeyEvent(view, keyCode)}
+    }
+
+    private fun fetchLocation() {
+        TODO("Not yet implemented")
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
