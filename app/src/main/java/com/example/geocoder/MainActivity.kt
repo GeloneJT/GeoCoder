1f7775ef0ef7d9de09b07cdba7fd5dd16cf23b9e
Jelani Thomas <jelani13@icloud.com>
2022-01-12 10:26:34 -0500

    permissions check added to MainActivity.kt

--- app/src/main/java/com/example/geocoder/MainActivity.kt
+++ app/src/main/java/com/example/geocoder/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.geocoder
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.app.ActivityCompat
 import com.example.geocoder.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,13 +19,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.searchButton.setOnClickListener{ fetchLocation()}
-        binding.enteredDestination.setOnKeyListener{ view, keyCode, _ ->handleKeyEvent(view, keyCode)}
+        binding.searchButton.setOnClickListener { fetchLocation() }
+        binding.enteredDestination.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            111
+        )
     }
 
     private fun fetchLocation() {
-        TODO("Not yet implemented")
+//        val location = binding.enteredDestination.text.toString()
+//        val geocoder = Geocoder(this, Locale.getDefault())
+//        val fromLocationName = geocoder.getFromLocationName(location, 1)
     }
+
     private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
             // Hide the keyboard
